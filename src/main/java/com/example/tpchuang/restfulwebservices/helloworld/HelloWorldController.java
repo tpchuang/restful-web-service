4bbd9f04bd942e21a6476993f5c752508a956b46
Troy P. Chuang <tpchuang@hotmail.com>
2024-11-19 18:27:41 -0800

    Add i18n message (step 18)

--- src/main/java/com/example/tpchuang/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/example/tpchuang/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,11 +1,17 @@
 package com.example.tpchuang.restfulwebservices.helloworld;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 public class HelloWorldController {
+
+  private final MessageSource messageSource;
 
   @GetMapping(path = "/hello-world")
   public String helloWorld() {
@@ -20,5 +26,11 @@ public class HelloWorldController {
   @GetMapping(path = "/hello-world-path-variable/{name}")
   public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
     return new HelloWorldBean(String.format("Hello %s!", name));
+  }
+
+  @GetMapping(path = "/greetings")
+  public String greetings() {
+    return messageSource.getMessage("good.morning.message", null, "Hi!",
+        LocaleContextHolder.getLocale());
   }
 }
--- src/test/java/com/example/tpchuang/restfulwebservices/helloworld/HelloWorldControllerTest.java
+++ src/test/java/com/example/tpchuang/restfulwebservices/helloworld/HelloWorldControllerTest.java
@@ -4,6 +4,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -39,5 +40,20 @@ class HelloWorldControllerTest {
     mockMvc.perform(MockMvcRequestBuilders.get("/hello-world-path-variable/" + name))
         .andExpect(MockMvcResultMatchers.status().isOk())
         .andExpect(jsonPath("$.message").value("Hello " + name + "!"));
+  }
+
+  @Test
+  void greetings() throws Exception {
+    mockMvc.perform(MockMvcRequestBuilders.get("/greetings"))
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(MockMvcResultMatchers.content().string("Good Morning!"));
+  }
+
+  @ParameterizedTest
+  @CsvSource({"en, Good Morning!", "nl, Goedemorgen!"})
+  void greetingsWithLocale(String locale, String expected) throws Exception {
+    mockMvc.perform(MockMvcRequestBuilders.get("/greetings").header("Accept-Language", locale))
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(MockMvcResultMatchers.content().string(expected));
   }
 }
